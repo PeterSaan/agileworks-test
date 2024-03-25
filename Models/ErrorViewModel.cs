f5b61c63c4ed96323935bc0cd9073b3cee5d0cf8
PeterSaan <saanpeter@gmail.com>
2024-03-25 19:38:51 +0200

    First commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace agileworks_test.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
